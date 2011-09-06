83ff617b325d578c6a6bc0ff4e8f4c50de23e767
Shaper <shaper.pmp@gmail.com>
2011-09-07 00:40:02 +0100

    Tidying up documentation

--- README.markdown
+++ README.markdown
@@ -2,7 +2,7 @@
 			
 **Coming soon...**
 			
-A library exposing CSS media query functionality to javascript, for by-media targeting of behaviour as well as style.  Anticipated to be especially useful in conjunction with [codomins](https://github.com/shaper-pmp/codomins), for by-media and by-browser-capability conditional loading of javascript (eg, for rich client widgets that are desired on graphical desktop machines, but which are too heavyweight or otherwise inappropriate for more limited/mobile devices).
+A library exposing CSS media query functionality to javascript, for by-media targeting of behaviour as well as style.  Anticipated to be especially useful in conjunction with [codomins](http://www.shaper-labs.com/projects/codomins/), for by-media and by-browser-capability conditional loading of javascript (eg, for rich client widgets that are desired on graphical desktop machines, but which are too heavyweight or otherwise inappropriate for more limited/mobile devices).
 			
 ##Status##
 			
@@ -34,4 +34,8 @@ Expected syntax:
 
 ##Work in progress##
 
-A very quick testing page (`mquery-test.html`) is included for convenience in the repository.
+A very quick testing page ([mquery-test.html](http://www.shaper-labs.com/projects/mquery/mquery-test.html)) is included for convenience in the repository.
+
+##Source Code##
+
+Work-in-progress mQuery source code is available [on github](http://www.github.com/shaper-pmp/mquery).
--- mquery.js
+++ mquery.js
@@ -1,7 +1,10 @@
+
 var mQuery = function () {
+
 
 	var test_div;
 	var style;
+	var debug=false;
 
 	return {
 		mediaTypesTestId: "mQuery-types-test",
@@ -72,29 +75,193 @@ var mQuery = function () {
 		},
 		
 		i: 0,
-		valid_tokens: ['\\,', 'and', 'or', 'not', '=', '\\>', '\\<', '\\>=', '\\<=', '\\(', '\\)'],
+		//valid_operators: [',', 'or', 'not', 'and', 'only', '=', '>', '<', '>=', '<=', '(', ')'],
+		valid_operators: ['(', ')', '*', '/', '+', '-'],
+		operator_precedence: {
+			'(': 1,
+			')': 1,
+			'*': 2,
+			'/': 2,
+			'+': 3,
+			'-': 3,
+		},
 		tokens: [],
+		tree: null,
 		
-		query: function(query) {
+		query: function(query, debug) {
+			this.debug = debug || false;
 			this.i = 0;
 			this.tokens = [];
-			/*var token = this.getNextToken(query, this.i, this.valid_tokens);
-			console.log(token);
-			if(token) {
-				this.i += token.end;
-			}*/
+			this.tree = null;
+
 			do {
-				var token = this.getNextToken(query, this.i, this.valid_tokens);
-				if(token != null) {
+				var token = this.getNextToken(query, this.i, this.valid_operators);
+				//console.log(token);
+				if(token != null && token != 'only') {	// Strip out "only"s, as they're only there to hide the rule from non-CSS3 browsers
 					this.tokens.push(token.token);
 					this.i += token.end;
 				}
 			}
 			while (token != null);
+			
+			this.tree = this.buildTree(this.tree, this.tokens);
+			if(this.tree) {
+				document.getElementById("mq_result").innerHTML += this.parseTree(this.tree);
+			}
+			
+			//console.log(this.tree);
+		},
+		
+		buildTree: function (root, tokens, depth) {
+			depth = depth || 0;
+			while(token = tokens.shift()) {
+				var newitem = this.makeASTNode(token);
+				
+				//console.log(newitem.value);
+				//if(!confirm(token)) break;
+				
+				if(root == null) {
+					if(newitem.type == 'value' || newitem.value == 'not') {	// Start empty trees with a leaf/unary operator
+						root = newitem;
+					}
+					else if(newitem.value == '(') {
+						var temp = null;
+						// Recursing for LHS
+						root = this.buildTree(temp, tokens, depth+1);
+						root.bracketed = true;
+					}
+					else {
+						console.log("Error!  Can't start expression with "+newitem.type+" '"+newitem.value+"'");
+					}
+				}
+				else if(root.type == "value") {
+					if(newitem.value == ")") {
+						// Returning from LHS
+						return root;
+					}
+					else if(newitem.type == "operator") {
+						newitem.leftchild = root;	root.parent = newitem;
+						root = newitem;
+					}
+					else {
+						console.log("Error!  value '"+newitem.value+"' can't follow value '"+root.value+"'");
+					}
+				}
+				else {	//  if(root.type == "operator")
+					if(newitem.type == "value") {	// Even unary NOTs will have their (single) child as their right child, for convenience
+						root.rightchild = newitem;
+						if(root.parent != null) {
+							root = root.parent;	// Pop back up from where we moved down a level (below)
+						}
+					}
+					else { 	// if(newitem.type == "operator")
+					
+						if(newitem.value == "(") {
+							var temp = null;
+							// Recursing for RHS
+							root.rightchild = this.buildTree(temp, tokens, depth+1);
+							root.rightchild.bracketed = true;
+						}
+						else if(newitem.value == ")") {
+							// Returning from RHS
+							return root;
+						}
+						
+						else if(this.operator_precedence[newitem.value] >= this.operator_precedence[root.value] || root.bracketed) {
+							newitem.leftchild = root;	root.parent = newitem;
+							root = newitem;	// Move down one level until we hit the next value
+						}
+						else {
+							console.log(newitem.value);
+							newitem.leftchild = root.rightchild;	root.rightchild.parent = newitem;
+							root.rightchild = newitem;	newitem.parent = root;
+							root = newitem;	// Move down one level until we hit the next value
+						}
+					}
+				}
+				
+				if(this.debug) {
+					this.outputTree(root, depth);
+				}
+
+			}
+			return root;
+		},
+		
+		parseTree: function(root) {
+		
+			if(root.type == 'value') {
+				return root.value;
+			}
+		
+			var leftval = Number(this.parseTree(root.leftchild));
+			var rightval =  Number(this.parseTree(root.rightchild));
+			
+			switch(root.value) {
+				case '*': 
+					return leftval * rightval;
+				case '/': 
+					return leftval / rightval;
+				case '+': 
+					return leftval + rightval;
+				case '-':
+					return leftval - rightval;
+			}
+		},
+		
+		makeASTNode: function (token) {
+			return {
+				"type": this.arrayContains(this.valid_operators, token) === false ? 'value': 'operator',
+				"value": token,
+				'leftchild': null,
+				'rightchild': null,
+				'parent' : null,
+				'bracketed': false
+			};
+		},
+		
+		outputTree: function(root, recursiondepth) {
+			var dbg = document.createElement("div");
+			dbg.style.marginBottom = "1em";
+			dbg.style.clear = "both";
+
+			var top = dbg;
+			while(recursiondepth > 0) {
+				var rec = document.createElement("div");
+				rec.className = "recurse";
+				rec.appendChild(top);
+				top = rec;
+				recursiondepth--;
+			}
+			document.getElementById("mq_result").appendChild(top);
+			dbg.innerHTML = this.renderTree(root)+'<hr />';
+		},
+		
+		renderTree: function(root) {
+			var output = '';
+			if(root == null) {
+				output = '';
+			}
+			else if(root.type == "value") {
+				output = '<div class="container"><div class="parent">'+root.value+'</div></div>';
+			}
+			else {
+				output = '<div class="container"><div class="parent">'+root.value+'</div>';
+				output += '<div class="child">'+this.renderTree(root.leftchild)+'</div>';
+				output += '<div class="child">'+this.renderTree(root.rightchild)+'</div></div>';
+			}
+			
+			
+			return output;
 		},
 		
 		getNextToken: function(raw_string, start, tokens) {
 			var token = '';
+			
+			re_escaped_tokens = [];
+			for(var i in tokens) {
+				re_escaped_tokens[i] = this.regexpEscape(tokens[i]);
+			}
 			
 			var padding = 0;
 			
@@ -102,7 +269,7 @@ var mQuery = function () {
 				padding++;
 			}
 			
-			if(possible = raw_string.substr(start+padding).match(RegExp("^(?:"+tokens.join("|")+")"))) {
+			if(possible = raw_string.substr(start+padding).match(RegExp("^(?:"+re_escaped_tokens.join("|")+")"))) {
 				return {
 					'token': possible[0],
 					'end': possible[0].length+padding
@@ -111,7 +278,7 @@ var mQuery = function () {
 				
 			for(var i=0; i+start<raw_string.length; i++) {
 				var c = raw_string[i+start+padding];
-				if((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_') {
+				if((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_'/* || c == '-'*/) {	// Temporarily disabled "-" while working out the expression parser
 					// else we haven't found the start of the token yet, so skip this character
 					token += c;
 				}
@@ -133,6 +300,22 @@ var mQuery = function () {
 			}
 			
 			return null;
+		},
+		
+		regexpEscape: function (text) {
+			var specials = [
+				'/', '.', '*', '+', '?', '|',
+				'(', ')', '[', ']', '{', '}', '\\'
+			];
+			var re = new RegExp('(\\'+specials.join('|\\')+')', 'g');
+			return text.replace(re, '\\$1');
+		},
+		
+		arrayContains: function (arr, obj) {
+			for(var i=0; i<arr.length; i++) {
+				if (arr[i] == obj) return i;
+			}
+			return false;
 		}
 	};
 }();
